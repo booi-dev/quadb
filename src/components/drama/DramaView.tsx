38e33e8d4262ff8f45b06d8b5dd0c1eb792108b3
Booi <56810550+booimangang@users.noreply.github.com>
2023-06-02 00:16:55 +0530

    Fix drama view

--- src/components/drama/DramaView.tsx
+++ src/components/drama/DramaView.tsx
@@ -26,38 +26,37 @@ function DramaView() {
   if (error) {
     return <div>Error: {error.message}</div>;
   }
-  if (data)
-    return (
-      <div className="drama-view">
-        <div className="app-flex">
-          <BackButton />
-          <GoHome />
-          <h1 className="app-title"> Drama Detail</h1>
+
+  return (
+    <div className="drama-view">
+      <div className="app-flex">
+        <BackButton />
+        <GoHome />
+        <h1 className="app-title"> Drama Detail</h1>
+      </div>
+
+      <h2 className="app-title drama-view__title"> '{data?.name}' Detail </h2>
+      <img src={data?.image.original} className="drama-view__image" />
+      {data && <p className="drama-view__summary"> {parser(data.summary)} </p>}
+      <div className="drama-view__more-info">
+        <div>
+          <b>Premiered</b> : {data?.premiered}{" "}
         </div>
-
-        <h2 className="app-title drama-view__title"> '{data?.name}' Detail </h2>
-        <img src={data?.image.original} className="drama-view__image" />
-        <p className="drama-view__summary"> {parser(data.summary)} </p>
-
-        <div className="drama-view__more-info">
-          <div>
-            <b>Premiered</b> : {data.premiered}{" "}
-          </div>
-          <div className="drama-view__genres">
-            <b>Genres </b> :
-            {data.genres.map((genre) => (
-              <div>{genre}</div>
-            ))}
-          </div>
-          <div>
-            <b> Rating </b> : {data.rating.average}{" "}
-          </div>
-          <div>
-            <b> Status </b> : {data.status}{" "}
-          </div>
+        <div className="drama-view__genres">
+          <b>Genres </b> :
+          {data?.genres.map((genre) => (
+            <div>{genre}</div>
+          ))}
+        </div>
+        <div>
+          <b> Rating </b> : {data?.rating.average}{" "}
+        </div>
+        <div>
+          <b> Status </b> : {data?.status}{" "}
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default DramaView;
